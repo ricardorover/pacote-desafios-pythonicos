1eeb5f2357769bfb95aba653ec6efaeeefceac18
Ricardo Rover <ricardo@joaorover.com>
2020-10-14 01:42:45 -0300

    Solve 02_both_ends

--- 02_both_ends.py
+++ 02_both_ends.py
@@ -7,10 +7,34 @@ Exemplo: 'spring' retorna 'spng'. Entretanto, se o tamanho da string
 for menor que 2, retorne uma string vazia.
 """
 
-def both_ends(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+def both_ends(s: str):
+    """
+    Resposta 1, inline
+    """
+    # return "" if len(s) < 2 else s[:2] + s[-2:]
 
+    """
+    Resposta 2, sem slice
+    """
+    # length = len(s)
+    # if length < 2:
+    #     return ""
+
+    # first = s[0] + s[1]
+    # last = s[length - 2] + s[length - 1]
+    # return first + last
+
+    """
+    Resposta 3, iterando
+    """
+    length = len(s)
+    if length < 2:
+        return ""
+
+    response = []
+    for i in (*range(0, 2), *range(length-2, length)):
+        response.append(s[i])
+    return "".join(response)
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
@@ -36,4 +60,5 @@ if __name__ == '__main__':
     test(both_ends, 'spring', 'spng')
     test(both_ends, 'Hello', 'Helo')
     test(both_ends, 'a', '')
+    test(both_ends, 'ab', 'abab') # novo teste sugerido pelo Gilson Filho
     test(both_ends, 'xyz', 'xyyz')

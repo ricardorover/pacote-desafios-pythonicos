5182392be74466449f7d0428f51e648e6435aadf
Ricardo Rover <rover@loeffa.com>
2020-10-19 01:27:08 -0300

    Solve 10_sort_last

--- 10_sort_last.py
+++ 10_sort_last.py
@@ -10,8 +10,31 @@ Irá retornar: [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 Dica: Use uma custom key= function para extrair o ultimo elemento de cada tupla.
 """
 def sort_last(tuples):
-    # +++ SUA SOLUÇÃO +++
-    return
+    # """ Resposta 1"""
+    # def sort(tuple):
+    #     return tuple[-1]
+    # return sorted(tuples, key=sort)
+
+    # """ Resposta 2"""
+    # return sorted(tuples, key=lambda tuple : tuple[-1])
+
+    # """ Resposta 3"""
+    # from operator import itemgetter
+    # return sorted(tuples, key=itemgetter(-1))
+
+    """ Resposta 4"""
+    def last_item_sort(tuple):
+        return tuple[-1]
+    return rover_sorted(tuples, key=last_item_sort)
+
+def rover_sorted(tuples, key=lambda item : item):
+    if not tuples:
+        return []
+    
+    items_values = list(map(key, tuples))
+    min_index = items_values.index(min(items_values))
+    min_item = tuples.pop(min_index)
+    return [min_item] + rover_sorted(tuples, key)
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -41,3 +64,5 @@ if __name__ == '__main__':
          [(3, 1), (1, 2), (2, 3)])
     test(sort_last, [(1, 7), (1, 3), (3, 4, 5), (2, 2)],
          [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+    test(sort_last, [(1, 7), (1, 3), (3, 4, 6), (2, 2), (1, 5)],
+         [(2, 2), (1, 3), (1, 5), (3, 4, 6), (1, 7)])

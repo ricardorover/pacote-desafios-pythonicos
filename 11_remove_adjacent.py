c7e8f79e25a35a8ff46c8d44cde5094ac22dad2f
Ricardo Rover <rover@loeffa.com>
2020-10-19 01:56:53 -0300

    Solve 11_remove_adjacent

--- 11_remove_adjacent.py
+++ 11_remove_adjacent.py
@@ -9,8 +9,25 @@ Irá retornar: [1, 2, 3]
 """
 
 def remove_adjacent(nums):
-    # +++ SUA SOLUÇÃO +++
-    return
+    # """ Resposta 1 """
+    # response = [nums[0]] if nums else []
+    # for index in range(1, len(nums)):
+    #     if nums[index - 1] != nums[index]:
+    #         response.append(nums[index])
+    
+    # return response
+
+    # """ Resposta 2 """
+    # adjacent = nums[1:] + [None]
+    # return [num for i, num in enumerate(nums) if num != adjacent[i]]
+
+    # """ Resposta 3 """
+    # adjacents = nums[1:] + [None]
+    # tuples = zip(nums, adjacents)
+    # return [tuple_[0] for tuple_ in tuples if tuple_[0] != tuple_[1]]
+
+    """ Resposta 4 """
+    return [tuple_[0] for tuple_ in zip(nums, nums[1:] + [None]) if tuple_[0] != tuple_[1]]
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
